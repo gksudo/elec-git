e2d88c49a1d18a9857bba4f3176fa325aead8c05
Bryce <brycewilliamse@gmail.com>
2018-02-15 15:43:43 -0500

    initial landing page

--- you_tried/functions/tasks.js
+++ you_tried/functions/tasks.js
@@ -1,5 +1,5 @@
 var test = "hi";
 var under = "subtxt"
 
-document.getElementById("lbl").innerHTML = test;
-document.getElementById("t2").innerHTML = under;
+// document.getElementById("lbl").innerHTML = test;
+// document.getElementById("t2").innerHTML = under;
