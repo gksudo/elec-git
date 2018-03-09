3d9f5d80aa4ab398fb6756e540809fd0eaad79c5
Bryce <brycewilliamse@gmail.com>
2018-03-09 00:25:56 -0500

    beginning functionality

--- you_tried/assets/functions/tasks.js
+++ you_tried/assets/functions/tasks.js
@@ -1,5 +1,18 @@
-var test = "hi";
-var under = "subtxt"
+function new_task()
+{
+  var txt = '<div class="panel panel-default">' +
+    '<div class="panel-heading">' +
+    '<h2 class="panel-title">' +
+    '<input type="text" id="title">' +
+    '<button style="float:right" class="btn btn-default" onclick="create_task(document.getElementById(\'title\').value)">Create</button>' +
+    '</h2>' +
+    '</div>' +
+    '</div>';
 
-// document.getElementById("lbl").innerHTML = test;
-// document.getElementById("t2").innerHTML = under;
+  $("#task-list").prepend(txt);
+}
+
+function create_task( title )
+{
+  alert(title);
+}
