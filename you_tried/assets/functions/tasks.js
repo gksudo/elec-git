80e35921e87bb4427d368780b0e3be3f2d4c53b5
Bryce <brycewilliamse@gmail.com>
2018-03-09 00:58:02 -0500

    adding new tasks

--- you_tried/assets/functions/tasks.js
+++ you_tried/assets/functions/tasks.js
@@ -1,18 +1,38 @@
 function new_task()
 {
-  var txt = '<div class="panel panel-default">' +
-    '<div class="panel-heading">' +
-    '<h2 class="panel-title">' +
-    '<input type="text" id="title">' +
-    '<button style="float:right" class="btn btn-default" onclick="create_task(document.getElementById(\'title\').value)">Create</button>' +
-    '</h2>' +
-    '</div>' +
-    '</div>';
+  var txt = '<div class="panel panel-default">'
+            + '<div class="panel-heading">'
+              + '<h2 class="panel-title">'
+                + '<div class="input-group">'
+                  + '<input type="text" id="title" class="form-control" placeholder="New Task">'
+                    + '<div class="input-group-btn">'
+                      + '<button id="ent" type="button" class="btn btn-default" onclick="create_task(document.getElementById(\'title\').value)">'
+                        +  '<b>+</b>'
+                      + '</button>'
+                    + '</div>'
+                  + '</h2>'
+                + '</div>'
+              + '</div>';
 
   $("#task-list").prepend(txt);
+
+  var title = document.getElementById("title");
+
+  // enter button
+  title.addEventListener("keyup", function(event)
+  {
+    event.preventDefault();
+    if (event.keyCode === 13)
+    {
+        document.getElementById("ent").click();
+    }
+  });
 }
 
+// TODO
 function create_task( title )
 {
-  alert(title);
+  console.log(title);
+
+  alert("up next");
 }
